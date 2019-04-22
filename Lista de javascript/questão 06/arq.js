fc71878806516f209c5073b24dcbc0b30973637b
kelvincrdz <47792299+kelvincrdz@users.noreply.github.com>
2019-04-22 17:39:07 -0300

    Atividade de javascript
    a atividade esta organizada de manira que cada questão tem uma pasta própria

---
+++ Lista de javascript/questão 06/arq.js
@@ -0,0 +1,7 @@
+  window.onclick = function avisoCaixa(evento) {
+    if (evento.target == document.getElementById("n1")) {
+    alert("Primeira caixa ativada!");
+  }
+    else{
+    alert("Perdeu o foco!");}
+  }
